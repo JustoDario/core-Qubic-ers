c722fa5471ad4671753ba3f4cfff41eef91f5ff0
Justo Dario <jd.valverde.2023@alumnos.urjc.es>
2025-03-22 22:29:01 +0100

    v1.1

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -20,9 +20,21 @@ public:
         uint64 numberOfBurnCalls;
     };
 
+    // Nuevas estructuras para setPrice
+    struct SetPrice_input
+    {
+        uint64 amountInEuros;
+        uint64 sellerIndex;  // Índice numérico para el vendedor
+    };
+    struct SetPrice_output
+    {
+        sint32 returnCode;
+    };
+
 private:
     uint64 numberOfEchoCalls;
     uint64 numberOfBurnCalls;
+    Array<uint64, 1024> sellerBalances;  // Array para almacenar los balances de los vendedores
 
     /**
     Send back the invocation amount
@@ -46,16 +58,28 @@ private:
         }
     _
 
+    /**
+    * Set price in euros for a seller
+    */
+    PUBLIC_PROCEDURE(SetPrice)
+        if (input.amountInEuros <= 0 || input.sellerIndex >= 1024)
+        {
+            output.returnCode = -1;
+            return;
+        }
+        state.sellerBalances.set(input.sellerIndex, input.amountInEuros);
+        output.returnCode = 0;
+    _
+
     PUBLIC_FUNCTION(GetStats)
         output.numberOfBurnCalls = state.numberOfBurnCalls;
         output.numberOfEchoCalls = state.numberOfEchoCalls;
     _
 
     REGISTER_USER_FUNCTIONS_AND_PROCEDURES
-
         REGISTER_USER_PROCEDURE(Echo, 1);
         REGISTER_USER_PROCEDURE(Burn, 2);
-
+        REGISTER_USER_PROCEDURE(SetPrice, 3);
         REGISTER_USER_FUNCTION(GetStats, 1);
     _
 
